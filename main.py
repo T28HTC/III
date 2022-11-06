3f041626bcb825ee83d30676d608eea967f0c052
movavi_school <zoyaterina@mail.ru>
2022-11-06 13:23:04 +0700

    work on lesson

---
+++ main.py
@@ -0,0 +1,14 @@
+# lst = ["ler", "mer", "ner", "non"]
+# for element in lst:
+#     print(element)
+#
+# for i in range(0, 10):
+#     print(i)
+
+# isgm = True
+# while isgm:
+#     i = input("Да/Нет ")
+#     if i == "Нет":
+#         isgm == False
+#
+#     print("sue")
